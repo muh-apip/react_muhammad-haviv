79e955edbc1a3763a6336579a6588c4ad3821e21
Muhammad Haviv <75481735+muh-apip@users.noreply.github.com>
2024-09-25 23:27:37 +0700

    pengumpulan tugas praktikum 3

---
+++ 03_Figma Introduction Interface Dimension/Praktikum/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        '2xl': '1440px',  // Menambahkan breakpoint untuk layar 1440px
+      },
+    },
+  },
+}
+
+
