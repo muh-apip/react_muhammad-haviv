a76600dcbfccb2fa899fcfb632f32c41078631a2
Muhammad Haviv <75481735+muh-apip@users.noreply.github.com>
2024-11-01 18:50:23 +0700

    pengumpulan tugas praktikum 20

--- 20_Authentication in React/Praktikum/src/components/Login.jsx
+++ 20_Authentication in React/Praktikum/src/components/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-blue-900 md:text-2xl">
+            <h1 className="text-xl font-bold text-center text-blue-900 md:text-2xl">
               Login
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
@@ -59,9 +59,9 @@ const Login = () => {
                   onChange={(e) => setPassword(e.target.value)}
                   required
                 />
+              <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-12 mb-12">Submit</button>
               </div>
               
-              <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Submit</button>
               {errorMessage && <p className="text-sm text-red-500">{errorMessage}</p>}
             </form>
           </div>
